1bddd79077781e75cc538d26ac364aa453640b1f
Santiago Pereira <s.pereira.insua@gmail.com>
2025-01-14 01:22:18 -0300

    cambios realizados 14/01 01:24

---
+++ src/components/LogoComponent.jsx
@@ -0,0 +1,9 @@
+import './styles/LogoComponent.css'
+
+export default function LogoComponent() {
+    return (
+        <>
+            <img className="img-logo" src="./src/assets/logo_2.png" alt="Logo de eCommerce ANUBIS"></img>
+        </>
+    )
+}
